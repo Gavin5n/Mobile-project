2af3ee4482076abf3bf0e0be5d86f70dbdf3df60
gavin <185272438@qq.com>
2020-09-09 19:01:20 +0800

    完成了项目准备工作

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
+import './styles/index.less'
+// 加载Vant组件
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+// 加载动态设置rem基准值
+import 'amfe-flexible'
+// 注册使用
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
